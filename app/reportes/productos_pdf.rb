44196f9ce5093fca932eda68919376c916aeadc8
ArturoLeonM <arturoleon0398>
2021-04-23 12:56:57 -0500

    Ultima version

--- app/reportes/productos_pdf.rb
+++ app/reportes/productos_pdf.rb
@@ -1,26 +1,46 @@
 class ProductosPdf
 
   def call
-
-
     Prawn::Document.new( 
-      page_size:'EXECUTIVE' 
-      
+      page_size:'EXECUTIVE'   
       )do
-        repeat :all do
+      stroke
 
-        bounding_box [bounds.left, bounds.top], :width  => bounds.width do
-          font "Helvetica"
-          text "Here's My Fancy Header", :align => :center, :size => 25
-          stroke_horizontal_rule
-        end
+      bounding_box [bounds.left, bounds.top], :width  => bounds.width do
+        font "Helvetica"
+        text "Reporte Productos", :align => :center, :size => 15
+        stroke_horizontal_rule
+      end
+
+      move_cursor_to 634
+
+      text_box 'Id', at: [15,cursor]
+      text_box 'Nombre', at: [35,cursor]
+      text_box 'Descripcion', at: [155,cursor]
+      text_box 'Precio', at: [300,cursor], align: :right, width: 50
+      text_box 'Tipo', at: [365,cursor]
+      move_down 15
+
+      Producto.all.each do |i|
+        # i.id,
+        # i.name,
+        # i.descripcion,
+        # i.precio,
+        # i.tipo.tipo
+
+
         
-        bounding_box [bounds.left, bounds.bottom + 28], :width  => bounds.width do
-          font "Helvetica"
-          stroke_horizontal_rule
-          move_down(5)
-          text "Este es un sexy footer", :size => 16
+        text_box i.id.to_s, at: [15,cursor]
+        text_box i.name, at: [35,cursor]
+        text = text_box i.descripcion, at: [155,cursor], width: 150, height: 15,  overflow: :truncate
+        text_box i.precio.to_s, at: [300,cursor], align: :right, width: 50
+        text_box i.tipo.tipo, at: [365,cursor]
+        move_down 15
+        while text.length > 0 do
+          text = text_box text, at: [155,cursor], width: 150, height: 15, overflow: :truncate
+          move_down 15
         end
+
       end
     end
   end
